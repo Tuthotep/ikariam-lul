1a85221c3098342effa0efed9a70b9dccb3adac9
Pablo Fernandez <149872748+Tuthotep@users.noreply.github.com>
2025-03-22 20:50:29 +0100

    agrego archivo

--- insert.sql
+++ insert.sql
@@ -1,166 +1,45 @@
-/tutotep/Luislanus/Hares/TITUSAGAIN/elmaspi/HitMan/Espartaco/Toreto/matmax/
-/yus2/Tor/The new one/SprayPaint/Drakego/TNT666/Doble J/KAFO6963/dark killer/
-/On The Aqueo/ginkgo/Nahuel91/Turco-cba/_Cristian_/Qatar/matias1118/Ivar deshuesado/
-/YMeLaPone
-INSERT INTO `t_ciudad` (`Id_ciudad`, `Ciudad`, `Comentarios_ciu`, `Idjugador`, `Idisla`)
+/tutotep/Luislanus/-escorpio68-/TITUSAGAIN/elmaspi/HitMan/Espartaco/Toreto/matmax/yus2//
+/Tor/The new one/SprayPaint/Drakego/TNT666/Doble J/KAFO6963/dark killer/On The Aqueo/ginkgo//
+/Nahuel91/Turco-cba/_Cristian_/Qatar/matias1118/Egea/YMeLaPone/Darkseid/Mariuss/kus classic//
+Deffman/Sogeking/Matrix/Hares/Erica/Burbujo LUL/Thomas Shelby/FEMCO6917/Renee/Ricardo L//
+burzaquero/Alvaro18dsc/Ivar deshuesado/Abusador/MARVEL/
+INSERT INTO 't_ciudad' ('Id_ciudad', 'Ciudad', 'Comentarios_ciu', 'Idjugador', 'Idisla')
 VALUES
 Ciudad-Idjugador-Id_isla
 (NULL, '', NULL, '', ''),
-(NULL, 'Mohan Poison', NULL, '165', '3');
+(NULL, 'Mohan Poison', NULL, '165', '3'),
 -----------------------------------------------------------
-INSERT INTO `t_isla` (`Id_isla`, `Isla`, `X`, `Y`, `Comentarios_isl`, `Idmateria`, `Idsector`, `Nivmateria`, `Nivbosque`)
+INSERT INTO 't_isla' ('Id_isla', 'Isla', 'X', 'Y', 'Comentarios_isl',
+'Idmateria', 'Idsector', 'Nivmateria', 'Nivbosque')
 VALUES
 (NULL, '', '', '', NULL, NULL, '', NULL, NULL),
-(NULL, 'Daraeos', '36', '50', NULL, NULL, '9', NULL, NULL);
+(NULL, 'Daraeos', '36', '50', NULL, NULL, '9', NULL, NULL),
 ===========================================================
-Renee/
-Friends Forever [82:33]
-Mirella [80:37]
-Alicia [81:32]
-Alexandra [81:32]
-Esmeralda [81:32]
-Ivanna [81:37]
-Narda [81:37]
-Nicoll [81:37]
-Nadia [81:42]
-Mabel [76:45]
-Maritza [76:45]
-Polis [93:33]
-FEMCO6917/
-LANUS [97:19]
-BANFIELD [97:19]
-BURZACO [97:19]
-MONTE GRANDE [98:20]
-LLAVALLOL [98:20]
-LOMAS DE ZAMORA [97:19]
-CANNING [97:15]
-KORN [97:15]
-EZEIZA [98:15]
-GLEW [98:15]
-ADROGUE [98:15]
-Polis [97:15]
-Thomas Shelby/
-Positano [95:52]
-Toscana [98:51]
-MerNegrTienda [98:51]
-Cinque Terre [97:51]
-Portofino [97:51]
-Manarola [96:49]
-Lauterbrunnen [95:52]
-Huacachina [95:52]
-Koh Panyi [98:52]
-Vernazza [97:51]
-Riomaggiore [97:51]
-Polis [24:62]
-Burbujo LUL/
-Cristalito [79:9]
-Sumeria [76:8]
-Burbujapolis [76:8]
-Clandestina [79:10]
-Lagash [79:10]
-La Nueva [79:10]
-Olivia [79:10]
-XXX [80:10]
-Infierno [80:10]
-Vinagre [76:8]
-Pocitos [79:10]
-Polis [77:12]
-Alvaro18dsc/
-Afrodita Piscis [94:4]
-Dohko Libra [94:19]
-Aioria Leo [95:16]
-Milo Scorpion [94:4]
-Kanon Geminis [94:4]
-Saga Geminis [94:19]
-Shaka Virgo [95:16]
-Mu Aries [95:16]
-Camus Acuario [95:16]
-Feder1973 [96:23]
-Shion Aries [96:23]
--escorpio68-/
-URA 2 [94:48]
-Syrah [97:49]
-Carrara [95:49]
-Marmolera [95:49]
-Bahía Blanca [95:49]
-Marmolin [95:49]
-URA 1 [94:48]
-Savignon [97:49]
-LaBahia [96:49]
-Polis01 [94:48]
-polisM [97:51]
-Matrix/
-Merovingio -MF- [91:14]
-Cypher -MF- [91:14]
-Seraph -MF- [91:14]
-Niobe -MP- [93:11]
-Persefone -VC- [97:11]
-Smith -VP- [90:6]
-Morfeo -CF- [91:30]
-Trinity -AF- [97:12]
-Neo -MF- [91:14]
-Oráculo - MP- [93:11]
-El Hacedor -MF- [91:14]
-Sogeking/
-Kabul [89:3]
-Atenas [90:3]
-Tegucigalpa [89:3]
-Ba Sing Se [84:17]
-Pekín [84:17]
-Kiev [84:17]
-Roma [86:17]
-Paris [86:17]
-Luanda [90:3]
-Yakarta [84:17]
-Polis [59:6]
-Deffman/
-Harrenhal F [99:42]
-Iron Islands F [96:44]
-Dreadfort F [96:44]
-Highgarden P [96:46]
-Kings Landing [96:45]
-Esteros [96:45]
-Westeros F [95:39]
-Casterly Rock [96:45]
-Riverrun P [96:46]
-Dragonstone F [95:39]
-Winterfell F [96:44]
-kus classic/
-Cimmeria [57:15]
-Vanaheim [57:11]
-Asgard [58:15]
-Argos [54:13]
-Nemedia [57:11]
-Turán [53:16]
-turky [57:18]
-Estygia [61:15]
-Hyperborea [59:18]
-Macho de ostro [53:13]
-Polis [69:20]
-Mariuss/
-Alepo 1 [89:43]
-Jerusalen 1 [92:48]
-Edesa 1 [92:47]
-Acre 2 [91:37]
-Antioquia 3 [91:11]
-Ascalon [91:54]
-Harrán [90:27]
-Tarso 3 [91:16]
-Turbessel [89:3]
-Jack 2 [91:36]
-Egipto 1 [92:41]
-Darkseid/
-Mármol C1 [91:52]
-Cristal H2 [91:51]
-Cristal H3 [92:47]
-Mármol C4 [91:52]
-Mármol C5 [91:52]
-Vino P6 [95:63]
-Mármol C7 [91:52]
-Vino P8 [100:66]
-Vino P9 [100:66]
-Vino P10 [100:66]
-Cristal H11 [91:51]   
+MAN-22/
+daom-53/
+Waly-35/
+Claypole-58/
+NeCrO-23/
+
+BARBABLANCA-56/
+BARBANEGRA-21/
+Angels Monic-60/
+Fabiano-41/
+X-MEN-45/
+
+NAZA_MAGNO-48/
+Dr Zactor-54/
+Romeneia-55/
+Angels Cat-57/
+Jason 62-59/
+
+Eternity Monic-62/
+marito OH-64/
+froliver-29/
+Carlos97-65/
+charly stark-63/
+Mitai vale-66
+
 ===========================================================
 ‖01|NO1|01/16-01/16‖‖02|NO2|17/32-01/16‖‖03|NO3|33/50-01/16‖
 ‖04|NO4|01/16-17/32‖‖05|NO5|17/32-17/32‖‖06|NO6|33/50-17/32‖
@@ -175,3 +54,7 @@ Cristal H11 [91:51]
 ‖28|SE1|51/67-51/67‖‖29|SE2|68/83-51/67‖‖30|SE3|84/100-51/67‖‖
 ‖31|SE4|51/67-68/83‖‖32|SE5|68/83-68/83‖‖33|SE6|84/100-68/83‖‖
 ‖34|SE7|51/67-84/100‖35|SE8|68/83-84/100‖36|SE9|84/100-84/100‖
+
+
+(NULL, 'Last Patrol', NULL, '46', '71')
+(NULL, 'Feder1973', NULL,'46','102')
